81abb5a0b1737f080f8da1b52a7cdcc3f09ce72e
Chris Gritter <16ctg@caslab.queensu.ca>
2019-11-07 19:11:39 -0500

    Hitbox header file iteration 1

---
+++ hitbox.h
@@ -0,0 +1,30 @@
+/*
+ * hitbox.h
+ *
+ *  Created on: Nov. 7, 2019
+ *      Author: Chris Gritter
+ */
+#pragma once
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <fstream>
+#include <typeinfo>
+#include <string>
+#include <ctype.h>
+
+using namespace std;
+
+class Hitbox{
+private:
+	float tx;
+	float ty;
+	float bx;
+	float by;
+public:
+	Hitbox();
+	Hitbox(float, float, float, float);
+	bool overlap(float, float, float, float);
+	void updateHitbox(float, float, float, float);
+};
